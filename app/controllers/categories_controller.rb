d89d02ae8ab952a35c4cb88f358396f36eaf2115
Lonely Wolf <pradeek.k@gmail.com>
2020-12-27 20:01:18 +0530

    added root routes as category#index, rectified like bugs, modifies readme file

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.includes(:products).all
   end
 
   # GET /categories/1
@@ -65,6 +65,7 @@ class CategoriesController < ApplicationController
     product = Product.find(params[:id])
     product.increment!(:like, 1)
     @like_count = product.like
+    @like_index = params[:id]
     respond_to do |format|
       format.html
       format.js
